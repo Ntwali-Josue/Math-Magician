4a8233d93a82a3b38d0a78fae2ba41aee2fea1cc
Ntwali-Josue <kntwalijos@gmail.com>
2021-11-24 21:51:11 +0200

    Create states and pass them as props in calculator component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
+import calculate from './logic/calculate';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      digitObj: {
+        total: null,
+        next: null,
+        operation: null,
+      },
+    };
+    this.calculator = this.calculator.bind(this);
   }
 
   render() {
     return (
       <div className="App">
-        <Calculator />
+        <Calculator digitObj={this.digitObj} />
       </div>
     );
   }
