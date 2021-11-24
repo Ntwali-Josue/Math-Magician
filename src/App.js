6dc73eae2b205d9b9c93293587ae546fe76f4acd
Ntwali-Josue <kntwalijos@gmail.com>
2021-11-24 07:56:04 +0200

    Import and Render Calculator component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import './App.css';
+import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Calculator />
+  </div>
+);
 
 export default App;
