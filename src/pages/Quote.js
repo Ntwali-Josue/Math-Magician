e042b7d4ccb1bb3f26c8c07d7f1b9d57488eded0
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-01 15:21:07 +0200

    Add quotes page

---
+++ src/pages/Quote.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ParticlesComponent from 'react-particles-js';
+
+const Quote = () => (
+  <div>
+    <div className="quotes">
+      <h3>
+        Mathematics is not about numbers, equations, computations, or algorithms.
+        <br />
+        It is about understanding. -William Paul Thurston.
+      </h3>
+    </div>
+    <ParticlesComponent />
+  </div>
+);
+
+export default Quote;
